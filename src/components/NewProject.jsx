890ab982572b8b870dda2c05b0f71e8322ac6bb8
Awais Madni <awais.madni17@gmail.com>
2024-08-22 19:34:52 +0500

    Collecting User Input with Refs & Forwarded

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,19 +1,47 @@
-import Input from "./Input.jsx"
+import { useRef } from "react";
+import Input from "./Input.jsx";
 
-export default function NewProject() {
-    return <div className="w-[35rem] mt-16">
-        <menu className="flex items-center justify-end gap-4 my-4">
-            <li>
-                <button className="text-stone-800 hover:text-stone-950">Cancel</button>
-            </li>
-            <li>
-                <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
-            </li>
-        </menu>
-        <div>
-            <Input label="Title" />
-            <Input label="Description" textarea />
-            <Input label="Due Date" />
-        </div>
+export default function NewProject({ onAdd }) {
+  const title = useRef();
+  const description = useRef();
+  const duaDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = duaDate.current.value;
+
+    // validation...
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
+  return (
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li>
+          <button className="text-stone-800 hover:text-stone-950">
+            Cancel
+          </button>
+        </li>
+        <li>
+          <button
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}
+          >
+            Save
+          </button>
+        </li>
+      </menu>
+      <div>
+        <Input type="text" label="Title" ref={title} />
+        <Input label="Description" textarea ref={description} />
+        <Input type="date" label="Due Date" ref={duaDate} />
+      </div>
     </div>
+  );
 }
